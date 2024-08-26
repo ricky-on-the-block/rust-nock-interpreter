3acd1e3c7894680ab565bab34eea3d1978ab55e0
Ric Waltman <ric.waltman@protonmail.com>
2024-08-26 12:41:49 -0700

    test: Add tests for tis (=) operation
    - Test the behavior of the tis function, which compares two Nouns for equality
    - Add test cases for:
      - Equal atoms
      - Unequal atoms
      - Equal cells
      - Unequal cells
      - Atom compared with cell
      - Equal nested cells

--- tests/operator-tests.rs
+++ tests/operator-tests.rs
@@ -46,8 +46,57 @@ fn test_lus_on_max_u64() {
 }
 
 #[test]
-#[should_panic()]
+#[should_panic(expected = "lus operation is not defined for cells, only atoms")]
 fn test_lus_on_cell() {
     let cell = Noun::Cell(Box::new(Noun::Atom(1)), Box::new(Noun::Atom(2)));
     lus(&cell);
+}
+
+// Test the Nock '=' operator, pronounced 'tis'
+#[test]
+fn test_tis_equal_atoms() {
+    let atom1 = Noun::Atom(42);
+    let atom2 = Noun::Atom(42);
+    assert_eq!(tis(&atom1, &atom2), Noun::Atom(0));
+}
+
+#[test]
+fn test_tis_unequal_atoms() {
+    let atom1 = Noun::Atom(42);
+    let atom2 = Noun::Atom(43);
+    assert_eq!(tis(&atom1, &atom2), Noun::Atom(1));
+}
+
+#[test]
+fn test_tis_equal_cells() {
+    let cell1 = Noun::Cell(Box::new(Noun::Atom(1)), Box::new(Noun::Atom(2)));
+    let cell2 = Noun::Cell(Box::new(Noun::Atom(1)), Box::new(Noun::Atom(2)));
+    assert_eq!(tis(&cell1, &cell2), Noun::Atom(0));
+}
+
+#[test]
+fn test_tis_unequal_cells() {
+    let cell1 = Noun::Cell(Box::new(Noun::Atom(1)), Box::new(Noun::Atom(2)));
+    let cell2 = Noun::Cell(Box::new(Noun::Atom(1)), Box::new(Noun::Atom(3)));
+    assert_eq!(tis(&cell1, &cell2), Noun::Atom(1));
+}
+
+#[test]
+fn test_tis_unequal_atom_and_cell() {
+    let atom = Noun::Atom(42);
+    let cell = Noun::Cell(Box::new(Noun::Atom(42)), Box::new(Noun::Atom(42)));
+    assert_eq!(tis(&atom, &cell), Noun::Atom(1));
+}
+
+#[test]
+fn test_tis_equal_nested_cells() {
+    let cell1 = Noun::Cell(
+        Box::new(Noun::Atom(1)),
+        Box::new(Noun::Cell(Box::new(Noun::Atom(2)), Box::new(Noun::Atom(3))))
+    );
+    let cell2 = Noun::Cell(
+        Box::new(Noun::Atom(1)),
+        Box::new(Noun::Cell(Box::new(Noun::Atom(2)), Box::new(Noun::Atom(3))))
+    );
+    assert_eq!(tis(&cell1, &cell2), Noun::Atom(0));
 }
